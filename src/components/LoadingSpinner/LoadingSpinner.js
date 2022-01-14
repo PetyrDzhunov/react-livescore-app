0b68bbca082f14d46895dd0a81b83a8b0c2883bc
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-01-14 18:00:40 +0200

    created my results to be in different colors depending on the result

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+	return (
+		<div className="box">
+			<div className="shadow"></div>
+			<div className="gravity">
+				<div className="ball"></div>
+			</div>
+		</div>
+	)
+}
+
+export default LoadingSpinner
--- src/containers/HomePage/LeagueTable.js
+++ src/containers/HomePage/LeagueTable.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import './LeagueTable.css';
 import getLeagueByName from '../../utils/getLeagueByName';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+
 
 const LeagueTable = () => {
 	const [league, setLeague] = useState();
@@ -23,38 +25,50 @@ const LeagueTable = () => {
 
 
 	return (
-		<table className='league-table'>
-			<thead>
-				<tr>
-					<td>Rank</td>
-					<td>Team</td>
-					<td>MP</td>
-					<td>W</td>
-					<td>D</td>
-					<td>L</td>
-					<td>G</td>
-					<td>PTS</td>
-					<td>Form</td>
-				</tr>
-			</thead>
-			<tbody>
-				{league?.map((team) => {
-					return (
-						<tr key={team.team.id}>
-							<td>{team.rank}</td>
-							<td>{team.team.name}</td>
-							<td>{team.all.played}</td>
-							<td>{team.all.win}</td>
-							<td>{team.all.draw}</td>
-							<td>{team.all.lose}</td>
-							<td>{team.all.goals.for} : {team.all.goals.against}</td>
-							<td>{team.points}</td>
-							<td>{team.form}</td>
-						</tr>
-					)
-				})}
-			</tbody>
-		</table>
+		<>
+			{isLoading && <LoadingSpinner />}
+			<h1>{league?.name}</h1>
+			<table className='league-table'>
+				<thead>
+					<tr>
+						<td>Rank</td>
+						<td>Team</td>
+						<td>MP</td>
+						<td>W</td>
+						<td>D</td>
+						<td>L</td>
+						<td>G</td>
+						<td>PTS</td>
+						<td>Form</td>
+					</tr>
+				</thead>
+				<tbody>
+					{league?.map((team) => {
+						return (
+							<tr key={team.team.id}>
+								<td>{team.rank}</td>
+								<td>{team.team.name}</td>
+								<td>{team.all.played}</td>
+								<td>{team.all.win}</td>
+								<td>{team.all.draw}</td>
+								<td>{team.all.lose}</td>
+								<td>{team.all.goals.for} : {team.all.goals.against}</td>
+								<td>{team.points}</td>
+								<td>{team.form.split().map((result, index) => <div key={index}>{result.split("").map((char, index) => (
+									<>{char === "W"
+										? <span key={index} className='win'>{char}</span>
+										: char === "L"
+											? <span key={index} className='lost'>{char}</span>
+											: <span key={index} className='draw'>{char}</span>}</>))}
+								</div>
+								)}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</>
+
 	)
 }
 
